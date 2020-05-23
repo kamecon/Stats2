a9085ae994f6cc9e225f19f8e58af977dd124d29
kamecon <kamalspain49@gmail.com>
2020-05-23 21:30:01 +0200

    minor changes

--- Plantilla tema 8.R
+++ Plantilla tema 8.R
@@ -1,10 +1,10 @@
 # Intervalos de confianza para la media muestral cuando la varianza es conocida
 
-n <- 16
-media <- 25
-dt <- 6
+n <- 
+media <-
+dt <- 
 
-confianza <- 0.95
+confianza <- 
 confianza2 <- (1-confianza)/2
 
 tabla <- qnorm(p = confianza2, lower.tail = FALSE)
@@ -19,17 +19,17 @@ LS
 
 # Intervalos de confianza para la media muestral cuando la varianza es desconocida
 
-n <- 25
-media <- 50
-s <- 8
+n <- 
+media <- 
+dt <- 
 
 gl <- n - 1
-confianza <- 0.95
+confianza <- 
 confianza2 <- (1-confianza)/2
 
 tabla <- qt(p = confianza2, df = gl, lower.tail = FALSE)
 
-ME <- tabla*(s/sqrt(n))
+ME <- tabla*(dt/sqrt(n))
 
 LI <- media - ME
 LS <- media + ME
@@ -39,11 +39,11 @@ LS
 
 # Intervalos de confianza para la proporcion muestral
 
-n <- 344
-proporcion <- 261/344
+n <- 
+proporcion <- /n
 dt <- sqrt((proporcion*(1-proporcion))/n)
 
-confianza <- 0.9
+confianza <- 
 confianza2 <- (1-confianza)/2
 
 tabla <- qnorm(p = confianza2, lower.tail = FALSE)
