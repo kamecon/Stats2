59ed97a75c39b36c9eb4b1e4736df70ee397096f
kamecon <kamalspain49@gmail.com>
2020-05-24 14:29:04 +0200

    fix graph for mean, not working for proportion

--- Funciones/intervalos.R
+++ Funciones/intervalos.R
@@ -16,7 +16,24 @@ tabla <- qnorm(p = confianza2, lower.tail = FALSE)
 ME <- tabla*(dt/sqrt(n))
 
 LI <- media - ME
-LS <- media + ME } else {
+LS <- media + ME 
+
+# Datos para el grafico
+
+# Creamos dos vectores, uno con los datos de la variable (empleo un vector que va desde la media menos 5 veces la desviación típica muestral hasta la media mas 5 veces la desviación típica muestral)
+
+lim_inf_graf <- media - 5*(dt / sqrt(n))
+lim_sup_graf <- media + 5*(dt / sqrt(n))
+
+x <- seq(from = lim_inf_graf, to = lim_sup_graf, by = 0.05 )
+
+# Creamos la distribución normal estándar basados en los datos creados en el paso anterior
+y <- dnorm((x - media) / (dt / sqrt(n)))
+
+# Juntamos ambos vectores y hacemos una tabla (data frame). La libreria ggplot solo toma como argumentos arreglos tabulates de este tipo
+df_dist <- data.frame(x=x, y=y)
+
+    } else {
 
 # Intervalos de confianza para la media muestral cuando la varianza es desconocida
 
@@ -31,7 +48,31 @@ ME <- tabla*(dt/sqrt(n))
 LI <- media - ME
 LS <- media + ME
 
-}
+# Datos para el grafico
+
+# Creamos dos vectores, uno con los datos de la variable (empleo un vector que va desde la media menos 5 veces la desviación típica muestral hasta la media mas 5 veces la desviación típica muestral)
+
+lim_inf_graf <- media - 5*(dt / sqrt(n))
+lim_sup_graf <- media + 5*(dt / sqrt(n))
+
+x <- seq(from = lim_inf_graf, to = lim_sup_graf, by = 0.05 )
+
+# Creamos la distribución normal estándar basados en los datos creados en el paso anterior
+y <- dt((x - media) / (dt / sqrt(n)), df = gl)
+
+# Juntamos ambos vectores y hacemos una tabla (data frame). La libreria ggplot solo toma como argumentos arreglos tabulates de este tipo
+df_dist <- data.frame(x=x, y=y)
+
+
+    }
+    
+    p_int <-  ggplot(df_dist, aes(x = x, y = y)) +
+      geom_line()  +
+      geom_vline(xintercept = LI) +
+      geom_vline(xintercept = LS) +
+      geom_area(mapping = aes(ifelse(x>LI & x<LS,x,0)), fill = "turquoise2", alpha = 0.8) +
+      xlim(lim_inf_graf,lim_sup_graf) +
+      annotate("text", x = media, y = 0.15, label = paste0(as.character(confianza*100), "% de confianza"))
     
   }
   
@@ -50,6 +91,30 @@ ME <- tabla*dt
 LI <- proporcion - ME
 LS <- proporcion + ME
 
+# Datos para el grafico
+
+# Creamos dos vectores, uno con los datos de la variable (empleo un vector que va desde la media menos 5 veces la desviación típica muestral hasta la media mas 5 veces la desviación típica muestral)
+
+lim_inf_graf <- proporcion - 5*dt
+lim_sup_graf <- proporcion + 5*dt
+
+x <- seq(from = lim_inf_graf, to = lim_sup_graf, by = 0.05 )
+
+# Creamos la distribución normal estándar basados en los datos creados en el paso anterior
+y <- dnorm((x - proporcion) / dt)
+
+# Juntamos ambos vectores y hacemos una tabla (data frame). La libreria ggplot solo toma como argumentos arreglos tabulates de este tipo
+df_dist <- data.frame(x=x, y=y)
+
+p_int <-  ggplot(df_dist, aes(x = x, y = y)) +
+  geom_line()  +
+  geom_vline(xintercept = LI) +
+  geom_vline(xintercept = LS) +
+  geom_area(mapping = aes(ifelse(x>LI & x<LS,x,0)), fill = "turquoise2", alpha = 0.8) +
+  xlim(lim_inf_graf,lim_sup_graf) +
+  annotate("text", x = proporcion, y = 0.15, label = paste0(as.character(confianza*100), "% de confianza"))
+
+
   }
   
   tablon <- data.frame(
@@ -63,40 +128,8 @@ LS <- proporcion + ME
   tablon[,1] <- NULL
   colnames(tablon) <- ""
   
-    # Creamos la distribución estándar basados en los datos creados en el paso anterior
-  
-  if(estadistico == "media"){
-    lim_inf_graf <- media - 5*(dt / sqrt(n))
-    lim_sup_graf <- media + 5*(dt / sqrt(n))
-    x <- seq(from = lim_inf_graf, to = lim_sup_graf, by = 0.05 )
     
-    if(CONOCIDA == TRUE){
-    y <- dnorm((x - media / (dt / sqrt(n))))} else{
-    y <- dt((x - media / (dt / sqrt(n))), df = gl)
-    } 
-  }
-    
-    if(estadistico == "proporcion"){
-      lim_inf_graf <- proporcion - 5*dt
-      lim_sup_graf <- proporcion + 5*dt
-      x <- seq(from = lim_inf_graf, to = lim_sup_graf, by = 0.05 )
-      y <- dnorm((x - proporcion / dt ))
-    }
-
-  
-  # Juntamos ambos vectores y hacemos una tabla (data frame). La libreria ggplot solo toma como argumentos arreglos tabulates de este tipo
-  df_dist <- data.frame(x=x, y=y)
-  
-  # Gráfico
-  pint <- ggplot(df_dist, aes(x = x, y = y)) +
-    geom_line()  +
-    geom_vline(xintercept = LI) +
-    geom_vline(xintercept = LS) +
-    geom_area(mapping = aes(ifelse(x>LI & x<LS,x,0)), fill = "turquoise2", alpha = 0.8) +
-    xlim(lim_inf_graf,lim_sup_graf) +
-    annotate("text", x = media, y = 0.15, label = paste0(as.character(confianza*100), "% de confianza"))
-  
-  return(list(Resumen =tablon,pint))
+  return(list(Resumen =tablon,p_int))
 
 }
 
@@ -106,3 +139,9 @@ intervalos(media = 18.68, dt = 1.69526, n = 24, CONOCIDA = FALSE, confianza = .9
 intervalos(media = 75, dt = 20,n = 64, CONOCIDA = TRUE, confianza = .95, estadistico = "media")
 # Ejemplo 7.6
 intervalos(proporcion = 0.759, n = 344, confianza = .9, estadistico = "proporcion")
+
+
+media = 18.68
+dt = 1.69526
+n = 24
+confianza = .9
