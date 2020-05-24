74d12c8ae05dfa9c151ab11449cf41f3abd7caf3
kamecon <kamalspain49@gmail.com>
2020-05-24 20:59:49 +0200

    add proportion table and function description, missing graphs

--- Funciones/intervalos2.R
+++ Funciones/intervalos2.R
@@ -1,5 +1,28 @@
 
 intervalos2 <- function(media_x, media_y, p_x, p_y, varianza_x, varianza_y, n_x, n_y, confianza, CONOCIDAS=TRUE, IGUALES=TRUE, estadistico = "media") {
+  
+  # Calcula intervalos de confianza para la diferencia de medias y proporciones
+  # Autor: Kamal Romero (karomero@ucm.es)
+  #
+  # Dependiencias: Necesita las librerias ggplot y gridExtra
+  # Args:
+  #   media_x: media muestra x
+  #   media_y: media muestra y
+  #   p_x: proporcion muestra x (debe ser un número estre cero y uno)
+  #   p_y: proporcion muestra y (debe ser un número estre cero y uno)
+  #   varianza_x: varianza muestra x
+  #   varianza_y: varianza muestra y
+  #   n_x: tamaño de la muestra x
+  #   n_y: tamaño de la muestra y
+  #   estadistico: el estadístico muestral, de ser "media" o "proporcion" (default = "media")
+  #   confianza: nivel de confianza del intervalo (debe ser un número estre cero y uno)
+  #   CONOCIDAS: TRUE si las varianzas son conocidas y FALSE en caso contrario (default TRUE)
+  #   IGUALES: TRUE si las varianzas son desconocidas e iguales y FALSE en caso contrario (default TRUE)
+  #
+  # Returns:
+  #   Una tabla con los límites del intervalo
+  #   Un gráfico con la distribución, los límites y la tabla
+  #
 
 
   if(estadistico == "media"){
@@ -91,6 +114,8 @@ LS <- diferencia + ME
 if(estadistico == "proporcion"){
   
 # Contrastes de la diferencia entre dos proporciones poblacionales 
+  
+diferencia_p <- p_x - p_y
 
 dt <- sqrt(((p_x*(1-p_x))/n_x) + ((p_y*(1-p_y))/n_y) )
 confianza2 <- (1 - confianza)/2
@@ -101,8 +126,8 @@ LI <- diferencia_p - ME
 LS <- diferencia_p + ME
 
 tablon <- data.frame(
-  "Concepto" = c("Nivel de confianza", "Margen de error", "Limite inferior", "Limite superior", "Estadístico"),
-  "Datos"=c(confianza, ME, LI, LS, ifelse(estadistico == "proporcion", proporcion, media))
+  "Concepto" = c("Nivel de confianza", "Limite inferior", "Limite superior", "Diferencia"),
+  "Datos"=c(confianza, LI, LS, diferencia_p)
 )
 
 
@@ -123,5 +148,7 @@ intervalos2(media_x = 3.08, media_y = 2.88, varianza_x = 0.42^2, varianza_y = 0.
 intervalos2(media_x = 133.3, media_y = 94, n_x = 10, n_y = 8, varianza_x = 218.011, varianza_y = 129.4256, confianza = .95, estadistico = "media",CONOCIDAS = FALSE, IGUALES = TRUE)
 #Ejemplo 8.5
 intervalos2(media_x = 290, media_y = 250, n_x = 16, n_y = 11, varianza_x = 15^2, varianza_y = 50^2, confianza = .95,  estadistico = "media", CONOCIDAS = FALSE, IGUALES = FALSE)
+#Ejemplo 8.6
+intervalos2(p_x = .892, p_y = .518, n_x = 120, n_y = 141, confianza = .95,  estadistico = "proporcion")
 
 
